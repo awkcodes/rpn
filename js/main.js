f53d37e1bba9f72a2664025aa343fbdbc53b995a
awk <abkhlaf1@gmail.com>
2023-04-21 07:22:01 +0300

    build js files and link to html

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
-const exp_numbers:number[] = [];
-
-function calculate(operator: string) 
-{
+"use strict";
+//import {rpnCalculator} from './index';
+// nodejs/cli interface
+//function readCli(){
+//    let args = process.argv.slice(2);
+//    let expression = args.join(' ');
+//    console.log(rpnCalculator(expression));
+//}
+//readCli()
+const exp_numbers = [];
+function calculate(operator) {
     const [b, a] = [exp_numbers.pop(), exp_numbers.pop()];
-    if (!b || !a) throw new Error('Invalid expression');
-    let result: number;
+    if (!b || !a)
+        throw new Error('Invalid expression');
+    let result;
     switch (operator) {
         case '+':
             result = a + b;
@@ -23,31 +31,27 @@ function calculate(operator: string)
     }
     exp_numbers.push(result);
 }
-
-function rpnCalculator(input: string) 
-{
+function rpnCalculator(input) {
     const tokens = input.split(' ');
-    for (let token of tokens){
-        !isNaN(parseFloat(token)) ?  exp_numbers.push(parseFloat(token)) :  calculate(token);
+    for (let token of tokens) {
+        !isNaN(parseFloat(token)) ? exp_numbers.push(parseFloat(token)) : calculate(token);
     }
     if (exp_numbers.length !== 1) {
         throw new Error('Invalid expression');
     }
     return exp_numbers.pop();
 }
-
-function getvalue() 
-{
-    const formInput = document.getElementById('rpn-input') as HTMLTextAreaElement;
+function getvalue() {
+    const formInput = document.getElementById('rpn-input');
     const rpnResult = document.getElementById('rpn-result') || document.createElement("h2");
     const expression = formInput.value;
-    let result: number|undefined;
+    let result;
     try {
         result = rpnCalculator(expression);
-    } catch(e) {
+    }
+    catch (e) {
         rpnResult.innerText = `error occured :: ${e}`;
         return;
     }
     rpnResult.innerText = `result is :: ${result}`;
 }
-
---
+++ src/cli.ts
@@ -0,0 +1,10 @@
+//import {rpnCalculator} from './index';
+
+// nodejs/cli interface
+//function readCli(){
+//    let args = process.argv.slice(2);
+//    let expression = args.join(' ');
+//    console.log(rpnCalculator(expression));
+//}
+
+//readCli()
